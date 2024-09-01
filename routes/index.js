47133847366afb0ef1cbe910415169e3d1d9bf54
julchu <jchumtl@gmail.com>
2024-09-01 19:01:18 -0400

    Initial Firebase auth and fetch setup

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var router = express.Router();
+
+const fetchUser = require("./../utils/firestore");
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./../teaworkapp-firebase-adminsdk-lfjb4-82c588d432.json");
+
+process.env["FIRESTORE_EMULATOR_HOST"] = "192.168.50.229:8080";
+process.env["FIREBASE_AUTH_EMULATOR_HOST"] = "192.168.50.229:9099";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+router.post("/", async (req, res) => {
+  const token = req.header("Authorization").split("Bearer ")[1];
+  console.log("token", token);
+  const user = await fetchUser(token);
+
+  if (user) res.send(user);
+  else res.send("nothing");
+});
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+module.exports = router;
---
+++ utils/firestore.js
@@ -0,0 +1,25 @@
+const { getAuth } = require("firebase-admin/auth");
+const { getFirestore } = require("firebase-admin/firestore");
+
+const fetchUser = async (token) => {
+  try {
+    const auth = await getAuth().verifyIdToken(token);
+    const userInfo = (
+      await getFirestore().doc(`users/${auth.user_id}`).get()
+    ).data();
+    // .collection("users").
+    // .get() // const userInfo = (
+    //   await getFirestore().collection("users").get(auth.user_id)
+    // ).docs.map((doc) => console.log(doc)); //.listDocuments(auth.user_id)
+
+    // console.log("auth", auth.user_id);
+    // console.log("userInfo", userInfo);
+
+    return userInfo;
+  } catch (error) {
+    console.log("error authorization", error);
+    return null;
+  }
+};
+
+module.exports = fetchUser;
